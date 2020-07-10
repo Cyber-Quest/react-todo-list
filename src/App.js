4e953abda831752f62d79b485772fe09fffcc175
Cyber-Quest <wesleu9983@gmail.com>
2020-07-10 15:22:57 -0300

    Finished aplication

--- src/App.js
+++ src/App.js
@@ -1,19 +1,94 @@
-import React from "react";
+import React, { Component } from "react";
+import { v4 as uuidv4 } from "uuid";
 import "bootstrap/dist/css/bootstrap.css";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
-function App() {
-  return (
-    <div>
-      <div class="container">
-        <div class="row">
-          <TodoInput>Vs test</TodoInput>
-          <TodoList></TodoList>
+class App extends Component {
+  state = {
+    items: [],
+    id: uuidv4(),
+    item: "",
+    editItem: false,
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      item: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const newItem = {
+      id: this.state.id,
+      title: this.state.item,
+    };
+    const updatedItens = [...this.state.items, newItem];
+    this.setState({
+      items: updatedItens,
+      item: "",
+      id: uuidv4(),
+      editItem: false,
+    });
+  };
+
+  clearList = (e) => {
+    this.setState({
+      items: [],
+    });
+  };
+
+  handleDelete = (id) => {
+    const filteredItems = this.state.items.filter((item) => item.id !== id);
+    this.setState({
+      items: filteredItems,
+    });
+  };
+
+  handleEdit = (id) => {
+    const filteredItems = this.state.items.filter((item) => item.id !== id);
+    const selectedItem = this.state.items.find((item) => item.id === id);
+    console.log(filteredItems);
+
+    this.setState({
+      items: filteredItems,
+      item: selectedItem.title,
+      id: id,
+      editItem: true,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="row">
+            <div className="col-10 mx-auto col-md-8 mt-5">
+              <h3 className="text-capitalize text-center">todo input</h3>
+
+              <TodoInput
+                item={this.state.item}
+                handleChange={this.handleChange}
+                handleSubmit={this.handleSubmit}
+                editItem={this.state.editItem}
+              >
+                Vs test
+              </TodoInput>
+
+              <TodoList
+                items={this.state.items}
+                clearList={this.clearList}
+                handleDelete={this.handleDelete}
+                handleEdit={this.handleEdit}
+              ></TodoList>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
